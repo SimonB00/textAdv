e0d33b1ae1fd1aedfcc24abc74041797b5e1a6ed
ameellea <mila.mazzeo95@gmail.com>
2022-07-29 16:29:54 +0200

    made a few changes

--- main.cc
+++ main.cc
@@ -1,10 +1,20 @@
 #include <iostream>
 #include <cassert>
-
+#include <string>
 #include "enemy.h"
 #include "map.h"
 #include "player.h"
 
 int main() {
-  
+std::cout << "Choose player name: " << '\n';
+std::string playername;
+std::cin >> playername;
+player name(playername);
+name.pickClass();
+name.getStats();
+name.getPlayerStatus();
+name.takeDamage(100);
+name.getPlayerStatus();
+name.heal(50);
+name.Attack();
 }
--- player.h
+++ player.h
@@ -17,88 +17,117 @@ private:
   std::string Class;
   int level_;
   int health_;
+  int maxHealth_;
   int stamina_;
+  bool died = false;
   stats playerStats = {10, 10, 10, 10};
 
 public:
+  std::string playerName;
+  player(std::string name) {
+    playerName = name;
+  };
   void pickClass() {
-    std::string choice;
-    std::cout << "Please pick a class:" << '\n';
-    std::cout << "WARRIOR" << '\n';
-    std::cout << "WIZARD" << '\n';
-    std::cout << "EXPLORER" << '\n';
-    std::cout << "OUTCAST" << '\n';
+    int choice;
+    std::cout << "Please pick a class (type number):" << '\n';
+    std::cout << "1 - WARRIOR" << '\n';
+    std::cout << "2 - WIZARD" << '\n';
+    std::cout << "3 - EXPLORER" << '\n';
+    std::cout << "4 - OUTCAST" << '\n';
     std::cin >> choice;
     
     switch(choice) {
-      case "WARRIOR":
-            Class = "WARRIOR"
+      case 1:
+            Class = "WARRIOR";
+            maxHealth_ = 150;
             health_ = 150;
             stamina_ = 200;
             playerStats.attack = 50;
             playerStats.defence = 100;
             playerStats.speed = 50;
             playerStats.mana = 10;
-            break
-      case "WIZARD":
-            Class = "WIZARD"
+            break;
+      case 2:
+            Class = "WIZARD";
+            maxHealth_ = 70;
             health_ = 70;
             stamina_ = 150;
             playerStats.attack = 150;
             playerStats.defence = 80;
             playerStats.speed = 100;
             playerStats.mana = 150;
-            break
-      case "EXPLORER":
-            Class = "EXPLORER"
+            break;
+      case 3:
+            Class = "EXPLORER";
+            maxHealth_ = 100;
             health_ = 100;
             stamina_ = 90;
             playerStats.attack = 40;
             playerStats.defence = 130;
             playerStats.speed = 200;
             playerStats.mana = 10;
-            break
-      case "OUTCAST":
-            Class = "OUTCAST"
+            break;
+      case 4:
+            Class = "OUTCAST";
+            maxHealth_ = 50;
             health_ = 50;
-            stamina_ = 50
+            stamina_ = 50;
             playerStats.attack = 50;
             playerStats.defence = 50;
             playerStats.speed = 50;
             playerStats.mana = 10;
-            break
+            break;
 
     };
   };
 
   void levelUp() {
     level_ += 1;
+    maxHealth_ += 10;
     playerStats.attack += 10;
     playerStats.defence += 10;
     playerStats.speed += 10;
     playerStats.mana += 10;
   };
 
-  int getHealth() {
-    return health_;
+  void getPlayerStatus() {
+    std::cout << "HEALTH:" << health_ << '\n';
+    std::cout << "STAMINA:" << stamina_ << '\n';
   };
 
   void getStats(){
-    std::cout << stamina_<< '\n';
-    std::cout << playerStats.attack << '\n';
-    std::cout << playerStats.defence << '\n';
-    std::cout << playerStats.speed << '\n';
-    std::cout << playerStats.mana << '\n';
+    std::cout << playerName << '\n';
+    std::cout << "CLASS:" << Class << '\n';
+    std::cout << "HEALTH:" <<  maxHealth_ << '\n';
+    std::cout << "STAMINA:" <<  stamina_<< '\n';
+    std::cout << "ATTACK:" <<  playerStats.attack << '\n';
+    std::cout << "DEFENCE:" <<  playerStats.defence << '\n';
+    std::cout << "SPEED:" <<  playerStats.speed << '\n';
+    std::cout << "MANA:" <<  playerStats.mana << '\n';
 
   };
-
+  
+  //correggere la matematica dubbia ovvero aggiungere difesa
   void takeDamage(int enemyAtk) {
     health_ -= (enemyAtk - playerStats.defence);
+
+    if(health_ == 0) {
+      died = true;
+    };
   };
 
   void heal(int healingValue) {
+    if(health_ < maxHealth_){
      health_ += healingValue;
+    } else if (health_ >= maxHealth_) {
+      health_ = maxHealth_;
+    };
+    std::cout << "You have " << health_ << " points" << '\n';
   };
+
+  int Attack() {
+    return playerStats.attack;
+   };
 
 };
 
